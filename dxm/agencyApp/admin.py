36e9f3704a3f9495bea1151b9330da778a98a84f
sixthfeel <7955230@qq.com>
2019-02-20 09:31:00 +0800

    office

--- dxm/agencyApp/admin.py
+++ dxm/agencyApp/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
 
 
 class AgencyInfoAdmin(admin.ModelAdmin):
-    list_display = ['agencyno', 'agencyname', 'agencytype', 'agencyonprd']
+    list_display = [
+        'agencyno', 'agencyname', 'agencytype', 'agencyonprd'
+    ]
+    list_filter = ['agencyno', 'agencyonprd']
+    search_fields = ['agencyno']
+    list_per_page = 5
 
 
 class TestInfoAdmin(admin.ModelAdmin):
-    list_display = ['testno', 'testappl', 'testappldate', 'testcont', 'testcontno', 'testcontmail', 'testtype',
-                    'teststartdate', 'testenddate', 'teststatus', 'agencyno']
+    list_display = [
+        'testno', 'testappl', 'testappldate', 'testcont', 'testcontno',
+        'testcontmail', 'testtype', 'teststartdate', 'testenddate',
+        'teststatus', 'agencyno'
+    ]
 
 
 admin.site.register(AgencyInfo, AgencyInfoAdmin)
